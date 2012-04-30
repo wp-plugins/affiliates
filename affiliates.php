01fea3a4635d141471ccd9de167ea176cf98530e
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-30 22:22:45 +0000

    missed to update 1.3.9
    git-svn-id: https://plugins.svn.wordpress.org/affiliates/tags/1.3.9@538356 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- affiliates.php
+++ affiliates.php
@@ -21,13 +21,13 @@
  * Plugin Name: Affiliates
  * Plugin URI: http://www.itthinx.com/plugins/affiliates
  * Description: The Affiliates plugin provides the right tools to maintain a partner referral program.
- * Version: 1.3.8
+ * Version: 1.3.9
  * Author: itthinx (Karim Rahimpur)
  * Author URI: http://www.itthinx.com
  * Donate-Link: http://www.itthinx.com
  * License: GPLv3
  */
-define( 'AFFILIATES_CORE_VERSION', '1.3.8' );
+define( 'AFFILIATES_CORE_VERSION', '1.3.9' );
 define( 'AFFILIATES_FILE', __FILE__ );
 if ( !defined( 'AFFILIATES_CORE_DIR' ) ) {
 	define( 'AFFILIATES_CORE_DIR', WP_PLUGIN_DIR . '/affiliates' );
